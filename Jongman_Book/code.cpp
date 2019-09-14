d86f3c9d35675cbb9630f9b99b7f63afce772f84
HVHO <hvho1119@gmail.com>
2019-09-14 21:59:07 +0900

    solve baekjoon P11729

--- Jongman_Book/code.cpp
+++ Jongman_Book/code.cpp
@@ -3,183 +3,83 @@
 #include <vector>
 #include <string>
 
+
 using namespace std;
 
+vector <pair<int,int> > list;
+vector< pair<int,int> > dp[22][4][4];
 
-int testCase = 0;
-
-vector<int> member;
-vector<int> fan;
-
-
-void iter_print(vector<int> &a) {
-
-    vector<int>::iterator it = a.begin();
-    while(it != a.end()) cout << *(it++);
-    cout << endl;
-
-}
-
-void nomarlize(vector<int> &a) {
-
-    for(int i = 0; i < a.size(); i++) {
-        
-        if(a[i] < 0) {
-
-            int borrow = ( abs(a[i]) + 9 ) / 10;
-
-            a[i+1] -= borrow;
-            a[i] += borrow * 10;
-
-        } else {
-
-            a[i+1] += a[i] / 10;
-            a[i] = a[i]%10;
-        }
-
-    }
-
-    while(a.size() > 1 && a.back() == 0) a.pop_back();
-
-}
-
-vector<int> multiply(vector<int> &a, vector<int> &b) {
-
-    vector<int> result(a.size() + b.size() + 1,0);
-
-    for(int i = 0; i < a.size(); i++)
-        for(int j = 0; j < b.size(); j++) {
-            result[i + j] += a[i]*b[j];
-        }
-
-    iter_print(result);
-
-    nomarlize(result);
-
-    return result;
-}
-
-// a += b*10^k
-void addTo(vector<int> &a, const vector<int> &b, int k) {
-
-    int an = a.size();
-    int bn = b.size();
-    int delta = an - bn - k;
-
-    // a 의 자릿수 조절
-    if ( delta < 0)
-        for(int i = 0; i < abs(delta); i++)
-            a.push_back(0);
-    
-    // a += b*10^k
-    for(int i = 0; i < bn; i++) {
-        a[i + k] += b[i];
-    }
-}
-
-// a -= b / assume a >= b
-void subFrom(vector<int> &a, const vector<int> &b) {
-
-    int an = a.size(), bn = b.size();
-
-    for(int i = 0; i < bn; i++) {
-        
-        a[i] -= b[i];
-
-        if(a[i] < 0) {
-            int borrow = ( abs(a[i]) + 9) / 10;
-            a[i + 1] -= borrow;
-            a[i] += borrow*10; 
-        }
-    }
-
-}
-
-
-vector<int> karatsuba (vector<int>& a, vector<int>& b) {
-
-    // assume an > bn
-    if(a.size() < b.size()) return karatsuba(b,a);
+void hanoi(int num, int dep, int via, int arr) {
 
     // base case
-    if(a.size() == 0 && b.size() == 0) return vector<int> ();
-    if(max(a.size(),b.size()) < 50) return multiply(a,b); 
 
-    // Divide and Conquer
+    if(num == 1) {
+        list.push_back(pair<int,int>(dep,arr));
+        return;
+    }
 
-    int half = a.size()/2;
+    if(num == 2) {
 
-    // a, b 를 반으로 쪼갠다
-    vector <int> a0(a.begin() , a.begin() + half);
-    vector <int> a1(a.begin() + half, a.end());
-    vector <int> b0(b.begin(), b.begin() + min<int>(b.size(), half));
-    vector <int> b1(b.begin()+min<int>(b.size(),half), b.end());
-
-    // z2 = a1*b1
-    vector <int> z2 = karatsuba(a1,b1);
-
-    // z0 = a0*b0
-    vector <int> z0 = karatsuba(a0,b0);
-
-    // a0 = a0 + a1
-    addTo(a0,a1,0);
-    // b0 = b0 + b1
-    addTo(b0,b1,0);
-    
-    // z1 = (a0 + a1)*(b0 + b1)
-    vector <int> z1 = karatsuba(a0,b0);
-
-    // z1 -= z2, z0
-    subFrom(z1,z2);
-    subFrom(z1,z0);
-
-    // ret = z0 + z1*10^half + z2*10^(half*2)
-    vector <int> result;
-    addTo(result,z0,0);
-    addTo(result,z1,half);
-    addTo(result,z2,2*half);
+        /*
+        cout << dep << " " << via << endl;
+        cout << dep << " " << arr << endl;
+        cout << via << " " << arr << endl;
+        */
+       list.push_back(pair<int,int>(dep,via));
+       list.push_back(pair<int,int>(dep,arr));
+       list.push_back(pair<int,int>(via,arr));
+        
+        return;
+    }
 
 
-    return result;
+    // divide and conquer
+
+    /*
+    hanoi(num - 1, dep, arr, via);
+    cout << dep << " " << via << endl;
+    hanoi(num - 1, via, dep, arr);
+    */
+
+   vector< pair<int,int> > &temp = dp[num][dep][arr];
+
+   if(!temp.empty()) {
+       //cout << "hit!!"<<endl;
+        list.insert(list.end(),temp.begin(),temp.end());
+   } else {
+        int start = list.size();
+        hanoi(num - 1, dep, arr, via);
+        list.push_back(pair<int,int>(dep,arr));
+        hanoi(num - 1, via, dep, arr);
+
+        temp.insert(temp.end(),list.begin() + start, list.end());
+   }
+   
+
+    return;
 
 }
-
 
 
 
 int main(void) {
 
-    int i, j;
+    int num;
+    cin >> num;
 
+    list.clear();
+    hanoi(num, 1, 2, 3);
 
-    cin >> testCase;
+    cout << list.size() << endl;
 
-    for(i = 0; i < testCase; i++) {
-        
-        vector<int> a,b;
-
-        char temp;
-        scanf("%c",&temp);
-
-        while(true) {
-            scanf("%c",&temp);
-            if(temp == 10) break;
-            a.push_back(temp - 48);
-        }
-        iter_print(a);
-        
-
-        while(true) {
-            scanf("%c",&temp);
-            if(temp == 10) break;
-            b.push_back(temp - 48);
-        }
-        iter_print(b);
-
-        vector <int> ret = karatsuba(a,b);
-
-        iter_print(ret);
-    }
+    /*
+    vector< pair<int,int> >::iterator it = list.begin();
+    while(it!=list.end()) cout << (*it).first << " " << (*it++).second << endl;
+    */
+   for(int i = 0; i < list.size(); i++) {
+       //cout << list[i].first << " " << list[i].second << "\n";
+       printf("%d %d\n",list[i].first,list[i].second);
+   }
 
     return 0;
 }
