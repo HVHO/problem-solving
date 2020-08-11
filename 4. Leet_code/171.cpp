6837e10b178fee37887907fc7745c4bd06bf603f
HVHO <hvho1119@gmail.com>
2020-08-11 21:50:47 +0900

    :seedling: Solve leetcode august challenge

---
+++ 4. Leet_code/171.cpp
@@ -0,0 +1,15 @@
+// 171. Excel Sheet Column Number
+
+class Solution {
+public:
+    int titleToNumber(string s) {
+        int res = 0;
+        
+        for(int i = 0; i < s.length(); i++) {
+            res *= 26;
+            res += s[i] - 'A' + 1;
+        }
+        
+        return res;
+    }
+};
