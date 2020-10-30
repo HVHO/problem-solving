f92a5c1db2abaa6c33ddbd9a5aad86b48ebcf12f
donghyun kim <hvho1119@gmail.com>
2020-10-30 17:18:17 +0900

    [WIP] solving swexpert week 2

--- 6.swexpert/week-2/user.cpp
+++ 6.swexpert/week-2/user.cpp
@@ -2,4 +2,25 @@ extern int check(unsigned char key[200]);
 
 void test(unsigned char key[200]) {
 
+    int candidate[256];
+    int number[200];
+    int pow[8] = {1, 2, 4, 8, 16, 32, 64, 128};
+
+
+    // search integer by it
+    // encoding number by adding number1 * 1 / number2 * 2 / number3 * 4 / number4 * 8 ..
+    // then we got right number by encoded with binary
+    // 1010 => number4 & number2 is right number
+    for(int i = 0; i < 37; i++) {
+        int idx = 0;
+        for(int j = 0; j < 7; j++) {
+            for(int k = 0; k < pow[j] && (7*i + j < 256); k++)
+                key[idx++] = 7*i + j;
+        }
+    }    
+    
+
+
+
+
 }
