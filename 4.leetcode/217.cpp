15d475772a04a41f1aa0e14d406b7c5654f41616
terry <hvho1119@gmail.com>
2021-02-10 16:39:13 +0900

    solve leetcode

---
+++ 4.leetcode/217.cpp
@@ -0,0 +1,16 @@
+// 	217. Contains Duplicate    
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+        set<int> s;
+        
+        for(int i: nums) {
+            if(s.find(i)!=s.end()) return true;
+            else s.insert(i);
+        }
+        
+        return false;
+    }
+};
---
+++ 4.leetcode/26.cpp
@@ -0,0 +1,20 @@
+// 26. Remove Duplicates from Sorted Array    
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int outputIdx = 0;
+        int prevNum = -10001;
+        for(int i = 0; i < nums.size(); i++) {
+            if(prevNum == nums[i]) {
+                continue;
+            } else {
+                prevNum = nums[i];
+                nums[outputIdx++] = nums[i];
+            }
+        }
+        
+        nums.resize(outputIdx);
+        return outputIdx;
+    }
+};
