c8d7356cbeb81394bb020b1550ab66821d1bb919
terry <hvho1119@gmail.com>
2021-03-23 22:08:39 +0900

    delete coding-interview section

--- main.cpp
+++ main.cpp
@@ -1,66 +1,93 @@
-#include <bits/stdc++.h>
 #include <iostream>
+#include <queue>
+#include <vector>
 
-#define MAX_PEOPLE  ((int)2e5 + 1)
 using namespace std;
 
-int root[MAX_PEOPLE];
-int _size[MAX_PEOPLE];
-int _rank[MAX_PEOPLE];
-// union find problem
-int find(int idx) {
-    if (root[idx] == idx) return idx;
-    return root[idx] = find(root[idx]);
-}
+struct cmp{
+	bool operator(pair<pair<double,char>,pair<int,int>> x, pair<pair<double,char>,pair<int,int>> y) {
+			if(x.first.first == y.first.first) {
+				if(x.second.first == y.second.first)
+					return x.second.second < y.second.second;
+				else
+					return x.second.first < y.second.first;
+			} else {
+				return x.first.first > y.first.first;
+			}
+	}
+};
 
-int _union(int l, int r) {
-   int lp = find(l);
-   int rp = find(r);
 
-    if(lp == rp) return _size[lp];
-
-    if (_rank[lp] < _rank[rp]) swap(lp, rp);
-
-    root[rp] = lp;
-    _size[lp] += _size[rp];
-    _size[rp] = 0;
-    if (_rank[lp] == _rank[rp]) _rank[lp]++;
-
-    return _size[lp];
-}
 
 
 int main() {
-    int TC;
-    scanf("%d", &TC);
-    while(TC--) {
-        int EC;
-        scanf("%d", &EC);
-        for(int i = 0; i < MAX_PEOPLE; i++) {
-            root[i] = i;
-            _rank[i] = 0;
-            _size[i] = 1;
-        }
-
-        map<string,int> idx_map;
-        int idx = 0;
-
-        for (int ec = 0; ec < EC; ec++) {
-            char lc[21], rc[21];
-            string l, r;
-            scanf("%s %s\n", lc, rc);
-            l = lc;
-            r = rc;
-
-            if(idx_map.find(l) == idx_map.end()) {
-                idx_map[l] = idx++;
-            }
-            if(idx_map.find(r) == idx_map.end()) {
-                idx_map[r] = idx++;
-            }
-            printf("%d\n", _union(idx_map[l],idx_map[r]));
-        }
-
-    }
-    return 0;
+	double like[5];
+	for(int i = 0; i < 5; i++) 
+		cin >> like[i];
+	
+	vector<pair<int,int>> opened;
+	vector<pair<int,int>> yet;
+	
+	int N,M;
+	cin >> N >> M;
+	
+	char buf;
+	for(int i = 0; i < N; i++) {
+		for(int j = 0; j < M; j++) {
+			scanf("%c", &buf);
+			if(buf == 'O') {
+				opened.push_back(make_pair(i,j));
+			} else if (buf == 'Y') {
+				yet.push_back(make_pair(i,j));
+			}
+		}
+		scanf("\n");
+	}
+	
+	char genre [101][101];
+	for(int i = 0; i < N; i++) {
+		for(int j = 0; j < M; j++) {
+			scanf("%c", &buf);
+			genre[i][j] = buf;
+		}
+		scanf("\n");
+	}
+	
+	priority_queue<
+		pair<pair<double,char>, pair<int,int>>,
+		vector<pair<pair<double,char>, pair<int,int>>>,
+		cmp> pq;
+	
+	for(auto e: yet) {
+		int n = e.first;
+		int m = e.second;
+		char g = genre[n][m];
+		double l = like[g - 'A'];
+		pq.push(make_pair(make_pair(l,g), make_pair(n,m)));
+	}
+	
+	while(!pq.empty()) {
+		auto e = pq.top();
+		pq.pop();
+		
+		printf("%c %.1f %d %d \n", e.first.first, e.first.second, e.second.first, e.second.second);
+	}
+	
+	for(auto e: opened) {
+		int n = e.first;
+		int m = e.second;
+		char g = genre[n][m];
+		double l = like[g - 'A'];
+		pq.push(make_pair(make_pair(l,g), make_pair(n,m)));
+	}
+	
+	while(!pq.empty()) {
+		auto e = pq.top();
+		pq.pop();
+		
+		printf("%c %.1f %d %d \n", e.first.first, e.first.second, e.second.first, e.second.second);
+	}
+	
+	
+	
 }
