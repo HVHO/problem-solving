24bb3fddd1c927b06a9c053d76d36e333bb2fd29
terry <hvho1119@gmail.com>
2021-08-05 15:11:15 +0900

    make main form

--- main.cpp
+++ main.cpp
@@ -15,76 +15,9 @@ using ull = unsigned long long;
 
 using namespace std;
 
-int N, L;
+int N;
 
-pair<int, int> deque[2 * NMAX]; // val, idx
-int f = 0, r = 0;
+int main(void) {
 
-bool empty()
-{
-    return f >= r;
-}
-
-void pop_front()
-{
-    f++;
-}
-
-void pop_rare()
-{
-    r--;
-}
-
-pair<int, int> front()
-{   
-    return deque[f + 1];
-}
-
-pair<int, int> rare()
-{
-    return deque[r];
-}
-
-void push_rare(pair<int, int> p)
-{
-    deque[++r] = p;
-}
-
-
-
-int sol()
-{
-    cin >> N >> L;
-    int input;
-    for (int i = 0; i < N; i++)
-    {
-        cin >> input;
-        if (!empty() && front().second <= (i - L))
-        {
-            // cout << "pop front : " << front().first << endl;
-            pop_front();
-        }
-        while (!empty() && rare().first > input)
-        // while (!dq.empty() && dq.back().first > input)
-        {
-            // cout << "pop rare : " << rare().first << endl;
-            pop_rare();
-            // dq.pop_back();
-        }
-        push_rare({input, i});
-        // dq.push_back({input, i});
-        cout << front().first << " ";
-        // cout << dq.front().first << " ";
-    }
-
-    return 0;
-}
-
-int main()
-{
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ios_base ::sync_with_stdio(false);
-    sol();
-    return 0;
+	return 0;
 }
