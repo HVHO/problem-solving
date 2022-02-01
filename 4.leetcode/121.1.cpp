ecd120491479f6794bc15f63b806a3f3b40e2cc0
terry <hvho1119@gmail.com>
2022-02-01 16:58:29 +0900

    solve leetcode feb challenge

---
+++ 4.leetcode/121.1.cpp
@@ -0,0 +1,15 @@
+// 121. Best Time to Buy and Sell Stock
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int res = 0;
+        int mn = prices[0];
+
+        for(int i = 1; i < prices.size(); i++) {
+            mn = min(mn,prices[i]);
+            res = max(res,prices[i]-mn);
+        }
+        return res;
+    }
+};
